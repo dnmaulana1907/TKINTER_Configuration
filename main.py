090a08072cb8567ea21816fa19d1371429ab426e
dnmaulana1907 <danimaulana1907@gmail.com>
2024-05-10 00:31:29 +0700

    Add quit and click button

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def add (*args):
-    return sum(args)
+import tkinter as tk
+from tkinter import ttk
+from termcolor import colored
 
-bilangan_prima = (1,2,3,5,7,11,13,17)
 
-print(add(*bilangan_prima))
+def Helloworld():
+    print(colored('Clicked', 'white', 'on_green'))
+    return
+
+def Quit():
+    print(colored('Quit', 'white', 'on_red'))
+    Window.destroy()
+
+Window = tk.Tk()
+
+button1 = ttk.Button(Window, text="Click", command= Helloworld)
+button1.pack(side="left", fill="both", expand=True)
+
+quit_button = ttk.Button(Window, text="Quit", command=Quit)
+quit_button.pack(side="bottom")
+
+Window.mainloop()
