bc152d8ff2dc01460e2f04f59824f850e1ee28b9
dnmaulana1907 <danimaulana1907@gmail.com>
2024-05-10 09:30:52 +0700

    Add label and textbox

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 from termcolor import colored
 
+clicked_count :int = 0
+# user_name:str= ""
 
-def Helloworld():
-    print(colored('Clicked', 'white', 'on_green'))
-    return
+def Count_clicked():
+    global clicked_count
+    print(colored(f'Clicked {clicked_count} and {user_name.get()}', 'white', 'on_green'))
+    clicked_count += 1
+    return clicked_count
 
 def Quit():
     print(colored('Quit', 'white', 'on_red'))
@@ -13,10 +17,19 @@ def Quit():
 
 Window = tk.Tk()
 
-button1 = ttk.Button(Window, text="Click", command= Helloworld)
-button1.pack(side="left", fill="both", expand=True)
+user_name = tk.StringVar()
+
+name_label = ttk.Label(Window, text="Name: ")
+name_label.pack(side="left", padx=(0, 10))
+
+name_entry = ttk.Entry(Window, width=15, textvariable= user_name)
+name_entry.pack(side="left")
+name_entry.focus()
+
+click_button = ttk.Button(Window, text="Click", command= Count_clicked)
+click_button.pack(side="left")
 
 quit_button = ttk.Button(Window, text="Quit", command=Quit)
-quit_button.pack(side="bottom")
+quit_button.pack(side="right")
 
 Window.mainloop()
